1f6990d27f402bdf482b19df2870c5e6fd548a9a
pas2rust <pas2000@proton.me>
2025-07-11 16:38:18 -0300

    Initialize Tailwind crate: start migrating components into separate crates

--- tailwind/target/debug/build/num-traits-b08923692296710d/build_script_build-b08923692296710d.d
+++
@@ -1,5 +0,0 @@
-/home/pas/Sources/crates/nekoui/tailwind/target/debug/build/num-traits-b08923692296710d/build_script_build-b08923692296710d.d: /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/pas/Sources/crates/nekoui/tailwind/target/debug/build/num-traits-b08923692296710d/build_script_build-b08923692296710d: /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs:
--- tailwind/target/debug/deps/iana_time_zone-208831b116a75c88.d
+++
@@ -1,9 +0,0 @@
-/home/pas/Sources/crates/nekoui/tailwind/target/debug/deps/iana_time_zone-208831b116a75c88.d: /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs
-
-/home/pas/Sources/crates/nekoui/tailwind/target/debug/deps/libiana_time_zone-208831b116a75c88.rlib: /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs
-
-/home/pas/Sources/crates/nekoui/tailwind/target/debug/deps/libiana_time_zone-208831b116a75c88.rmeta: /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs /home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs
-
-/home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs:
-/home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs:
-/home/pas/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs:
